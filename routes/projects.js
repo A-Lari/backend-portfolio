abbec428185e90b0524ba2eb73cbad972ad674c4
A-Lari <lari.anthony84@gmail.com>
2022-04-25 16:47:49 +0200

    add searchtitle v1

--- routes/projects.js
+++ routes/projects.js
@@ -1,13 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const projectController = require('../controllers/projects');
+const projectController = require("../controllers/projects");
 
 const { checkAuth } = require("./checkAuth");
 
 /* GET projects listing. */
+
+router.get("/search/:title", projectController.searchByTitle);
 router.get("/", projectController.getProjects);
 router.get("/:idProject", projectController.getProject);
+
 router.post("/:idProject/likes", projectController.addLikes);
 router.post("/", checkAuth, projectController.createProject);
 

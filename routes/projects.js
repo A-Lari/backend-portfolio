088e2360c8a6413fe63b2c61aedda3b34d45ec37
Christian ly <christian_ly@outlook.fr>
2022-04-25 10:55:56 +0200

    Ajout routes projets + auth

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const projectController = require('../controllers/projects')
+const projectController = require('../controllers/projects');
+
+const { checkAuth } = require("./checkAuth");
 
 /* GET projects listing. */
 router.get("/", projectController.getProjects);
+router.get("/:idProject", projectController.getProject);
+router.post("/:idProject/likes", projectController.addLikes);
+router.post("/", checkAuth, projectController.createProject);
 
 module.exports = router;

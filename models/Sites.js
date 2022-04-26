233522c418e37ebf2b329f93e6dc2e469ecd8fb7
Christian ly <christian_ly@outlook.fr>
2022-04-26 15:13:50 +0200

    Ajoute route delete site + modif model site

--- models/Sites.js
+++ models/Sites.js
@@ -4,6 +4,7 @@ const SiteSchema = new mongoose.Schema({
   title: String,
   logo: String,
   footer: String,
+  presentation_titre: String,
   presentation_sum: String,
   background_img: String,
 }, { timestamps: true });
